77e9b5a7e871a619fae88d13e380b9a8042ec056
icyfox <34009674@qq.com>
2014-04-25 18:39:43 +0800

    更改文字

--- auto.py
+++ auto.py
@@ -2,14 +2,13 @@
 # -*- coding: UTF-8 -*-
 
 import sys,os,re
-from CopyArticle import copyArticle
 
 ID=long(sys.argv[0])
 if not ID:
 	sys.exit(1)
 
 j=1
-content='\xd6\xd0\xb9\xfa'
+content='\xc7\xeb\xb7\xa2\xb5\xbd \xb5\xe7\xc4\xd4\xca\xfd\xc2\xeb\xbd\xbb\xd2\xd7 \xb0\xe6'
 data=['r','r', '\n', content, '\x17','\n','b', '6', '\n', '\n' 'b','7','\n','\n']
 
 for item in data:
